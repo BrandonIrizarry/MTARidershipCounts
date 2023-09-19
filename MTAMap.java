2887f4d59cba629485f5375145eb4838aa409f52
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2023-09-18 20:08:24 -0400

    Draw markers based on a day's worth of data

--- MTAMap.java
+++ MTAMap.java
@@ -6,23 +6,47 @@ import java.util.List;
 
 import de.fhpotsdam.unfolding.data.PointFeature;
 import de.fhpotsdam.unfolding.geo.Location;
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.marker.Marker;
+import de.fhpotsdam.unfolding.marker.SimplePointMarker;
+import de.fhpotsdam.unfolding.utils.MapUtils;
+
 import processing.core.PApplet;
 
 public class MTAMap extends PApplet {
-    public MTAMap(List<PointFeature> pointFeatures) {
+    private UnfoldingMap map;
+    private List<Marker> subwayMarkers;
 
+    public MTAMap(List<PointFeature> pointFeatures) {
+        subwayMarkers = new ArrayList<>();
+
+        for (PointFeature pointFeature : pointFeatures) {
+            SubwayMarker subwayMarker = new SubwayMarker(pointFeature);
+            subwayMarkers.add(subwayMarker);
+        }
     }
 
     public void setup() {
+        int mapWidth = 800;
+        int mapHeight = 600;
+        int offset = 50;
 
+        size(mapWidth, mapHeight, OPENGL);
+
+        map = new UnfoldingMap(this, offset, offset, mapWidth - offset, mapHeight - offset);
+        MapUtils.createDefaultEventDispatcher(this, map);
+
+        map.addMarkers(subwayMarkers);
     }
 
     public void draw() {
-
+        background(200);
+        map.draw();
     }
 
     public static void main(String[] args) {
-        List<PointFeature> pointFeatures = Parser.parse("data/jan-2023.csv");
+        String dataFilename = args[0];
+        List<PointFeature> pointFeatures = Parser.parse(dataFilename);
 
         MTAMap app = new MTAMap(pointFeatures);
         PApplet.runSketch(new String[]{"MTAMap"}, app);
