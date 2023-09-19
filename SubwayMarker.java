f611bc796e568b411e07f4ee846439f0f3d1325d
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2023-09-18 21:38:30 -0400

    WIP: add ridership count to tooltip
    The aesthetics need to be improved, though

--- SubwayMarker.java
+++ SubwayMarker.java
@@ -18,18 +18,30 @@ public class SubwayMarker extends CommonMarker {
     @Override
     public void showTitle(PGraphics pg, float x, float y) {
         String stationComplex = (String)getProperty("station_complex");
+        String totalRidership = (String)getProperty("total_ridership");
 
         pg.pushStyle();
 
         pg.rectMode(PConstants.CORNER);
 
+        int xOffset = 0;
+        int yOffset = 15;
+        int textMarginX = 3;
+        int textMarginY = 3;
+        int boxHeight = 50;
+        int cornerRadius = 5;
+        int horizontalPadding = 2 * textMarginX;
+        int boxWidth = (int)Math.max(pg.textWidth(stationComplex), pg.textWidth(totalRidership)) + horizontalPadding;
+        int lineHeight = 15;
+
         pg.stroke(110);
         pg.fill(255, 255, 0);
-        pg.rect(x, y + 15, pg.textWidth(stationComplex) +6, 18, 5);
+        pg.rect(x + xOffset, y + yOffset, boxWidth, boxHeight, cornerRadius);
 
         pg.textAlign(PConstants.LEFT, PConstants.TOP);
         pg.fill(0);
-        pg.text(stationComplex, x + 3 , y +18);
+        pg.text(stationComplex, x + xOffset + textMarginX, y + yOffset + textMarginY);
+        pg.text(totalRidership, x + xOffset + textMarginX, y + yOffset + textMarginY + lineHeight);
 
         pg.popStyle();
     }
